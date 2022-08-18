efd4261bc6be5f88d0a7ca4737fd98be7821e63f
MD ABDULLAH <MD ABDULLAH@LAPTOP-Q63U815G>
2022-08-18 12:42:15 +0530

    first commit

---
+++ B22FundamentalProjects/src/Scope.java
@@ -0,0 +1,21 @@
+
+public class Scope {
+
+	public static void main(String[] args) {
+		 int outer=1;
+		 {
+			 int inner=2;
+			 System.out.println(outer); //1
+			 System.out.println(inner); //2
+			 
+		 }
+		 int inner=3;
+		 System.out.println(inner); //3
+		 System.out.println(outer); //1
+		 	
+		 
+		Scope a = new Scope();
+
+	}
+
+}
