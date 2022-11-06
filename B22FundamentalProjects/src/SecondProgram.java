310a677e42c907d94389081cb7af28d1d1e717d6
MD ABDULLAH <MD ABDULLAH@LAPTOP-Q63U815G>
2022-11-07 00:08:56 +0530

    first commit

--- B22FundamentalProjects/src/SecondProgram.java
+++ B22FundamentalProjects/src/SecondProgram.java
@@ -8,12 +8,25 @@ public class SecondProgram {
 		int namC=0;
 		
 		//I am going to do some operation before printing this value
-		int namD=--namC + namB--; // (--0 PRE-DECREMENT =-1) + (10++POST-DECREMENT= 10 only)=9
-		System.out.println(namC); // (--0 PRE-DECREMENT =-1)
+		int namD=--namC + namB--; // (--0 PRE-DECREMENT (0-1) =-1) + (10++POST-DECREMENT= 10 only)=9
+		
 		//--namC (before variable is called as PRE-DECREMENT)
-		System.out.println(namB); // (10++ POST-DECREMENT 10-1=9)
+		System.out.println(namC); // (--0 PRE-DECREMENT (0-1) =-1))
+		
 		//namB-- (after variable is called as POST-DECREMENT)
-		System.out.println(namD); // 9
+		System.out.println(namB); // (10++ POST-DECREMENT (10-1)=9))
+		
+		int namE=++namB + namA++;
+		
+		//++namB (before the variable is called as PRE-INCREMENT)
+		System.out.println(namB); // (10 PRE-INCREMENT =10 only)
+		
+		//namA++ (after the variable is called as POST-INCREMENT)
+		System.out.println(namA); // (5 POST-INCREMENT 5+1=6)
+		
+		System.out.println(namE); //15; (10+5)
+		
+		System.out.println(namD); // 9; (-1 + 10)
 
 	}
 
--- B22FundamentalProjects/src/ThirdProgram.java
+++ B22FundamentalProjects/src/ThirdProgram.java
@@ -8,13 +8,15 @@ public class ThirdProgram {
 		int namC=11;
 	
 		//I am going to do some operation before printing this value
-		int namE=++namB + namA++; // (10+1 PRE_INCREMENT=11, + 5 POST-INCREMENT= 16)
-	     System.out.println(namB); // (10+1PRE-INCREMENT=11)
-	     //++namB (before the variable is called as PRE-INCREMENT)
-	     System.out.println(namA); // (5 POST-INCREMENT 5+1=6)
-	     //namA++ (after the variable is called as POST-INCREMENT)
-	     System.out.println(namE); //16
+		int namE=++namB + namA++; // (10 PRE_INCREMENT (10+1)=11) + (5 POST-INCREMENT)= 16))
 		
+		//++namB (before the variable is called as PRE-INCREMENT)
+	     System.out.println(namB); // (10 PRE-INCREMENT (10+1)=11)
+	     
+	   //namA++ (after the variable is called as POST-INCREMENT)
+	     System.out.println(namA); // (5 POST-INCREMENT 5+1=6)
+	     
+	     System.out.println(namE); //16 (10+6)
 
 	}
 
