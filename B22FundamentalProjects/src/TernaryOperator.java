07c8f112621392270e2f98b38c89cf0c4b265402
MD ABDULLAH <MD ABDULLAH@LAPTOP-Q63U815G>
2022-11-07 11:57:20 +0530

    fisrt commit

--- B22FundamentalProjects/src/TernaryOperator.java
+++ B22FundamentalProjects/src/TernaryOperator.java
@@ -4,12 +4,13 @@ public class TernaryOperator {
 	public static void main(String[] args) {
 		
 		int v=1;
-  //Whenever you have to question mark(?)and colon(:) together then that's a Ternary Operator
-  //if your output is TRUE, before your question mark(?),Then Consider the value before a colon(:)
+  //Whenever you have to this question mark(?) and colon(:) together then that's a Ternary Operator
+  // What is ternary Operator will do? it's checks the value before your Question mark(?)and it given boolean output
+  //Tell me V==1 is it true/False = True, if your output is TRUE, before your question mark(?),Then Consider the value before a colon(:)
   //Output of the first printing statement is A. 
 		System.out.println(v==1?"A":"B"); //T? //A
-  //if your output is FALSE, after your question mark(?), Then consider the value after a colon(:)
-		//Output of the Second printing statement is B.
+  //tell me V==2 id it True/false =False, if your output is FALSE, after your question mark(?), Then consider the value after a colon(:)
+  //Output of the Second printing statement is B.
 		System.out.println(v==2?"A":"B"); //F? //B
 
 	}
