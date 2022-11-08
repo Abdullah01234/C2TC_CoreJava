bb287a8e9ed8db612aef5ba3213e774ec663a538
MD ABDULLAH <MD ABDULLAH@LAPTOP-Q63U815G>
2022-11-08 22:42:16 +0530

    first commit

--- B22FundamentalProjects/src/RelationalLogicalOperator.java
+++ B22FundamentalProjects/src/RelationalLogicalOperator.java
@@ -2,7 +2,10 @@ import java.util.Random;
 public class RelationalLogicalOperator {
 
 	public static void main(String[] args) {
+		//You are creating an object.So,java is going to be a object oriented programming language.So for each every class we will be creating object.
+		//How will be create object? new keyword will help you to create object.
 		Random rand = new Random();
+		
 		int i =rand.nextInt(100);
 		int j =rand.nextInt(100);
 		System.out.println("i:" + i); //35
