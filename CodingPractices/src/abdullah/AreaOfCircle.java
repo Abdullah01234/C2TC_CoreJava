7d15d0248104425aba8a6dc9ab58d1ed3c40e329
MD ABDULLAH <MD ABDULLAH@LAPTOP-Q63U815G>
2022-11-19 22:25:01 +0530

    first commit

---
+++ CodingPractices/src/abdullah/AreaOfCircle.java
@@ -0,0 +1,17 @@
+package abdullah;
+
+import java.util.Scanner;
+
+    public static final float PI= 3.14f;
+    static void main(String[] args) {
+    		
+    Scanner sc = new Scanner(System.in);
+		int a =10;
+		System.out.println("Enter the Area of Circle");
+		float f = sc.nextFloat();
+		System.out.println("Area of Circle is :" + PI*f*f);
+sc.close();	
+
+	}
+
+}
---
+++ CodingPractices/src/abdullah/Program1.java
@@ -0,0 +1,38 @@
+package abdullah;
+ class Program1{
+//driver code
+
+public static void main(String args[])
+
+{
+
+
+
+    for(int a = 1; a<=4; a++)
+
+    {
+
+
+
+    System.out.println(Math.pow(a, 2));
+
+
+
+  } 
+
+    for(int a = 1; a<=4; a++)
+
+  {
+
+  System.out.println(Math.pow(a, 2));
+
+  }
+
+ 
+
+}
+
+}
+	
+
+
