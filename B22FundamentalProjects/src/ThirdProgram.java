84aa44829d743e572c42566a75e14d6bf149eab4
MD ABDULLAH <MD ABDULLAH@LAPTOP-Q63U815G>
2022-11-20 15:13:05 +0530

    first commit

--- B22FundamentalProjects/src/ThirdProgram.java
+++ B22FundamentalProjects/src/ThirdProgram.java
@@ -6,18 +6,19 @@ public class ThirdProgram {
 		int namA=5;
 		int namB=10;
 		int namC=11;
-	
-		//I am going to do some operation before printing this value
-		int namE=++namB + namA++; // (10 PRE_INCREMENT (10+1)=11) + (5 POST-INCREMENT)= 16))
+//Note:- Whenever you have the increment and decrement before your variable or after the variable, How does it works?
+//if you have before your variable (++numB) the operation will be taken first (++) then the value (numB) will be perform.
+//if you have after your variable (numA++) the value will be taken first (numA) then the operation (++) will be perform.
+		//I am doing to do some operation before and after printing this value
+		int namE=++namB + namA++; // (10 PRE_INCREMENT (10 only) + (5 POST-INCREMENT) (5+1) 6) (10 + 6 = 16)
 		
 		//++namB (before the variable is called as PRE-INCREMENT)
-	     System.out.println(namB); // (10 PRE-INCREMENT (10+1)=11)
+	     System.out.println(namB); // (10 PRE-INCREMENT 10+1 = 11)
 	     
 	   //namA++ (after the variable is called as POST-INCREMENT)
-	     System.out.println(namA); // (5 POST-INCREMENT 5+1=6)
+	     System.out.println(namA); // (5 POST-INCREMENT 5+1 = 6)
 	     
-	     System.out.println(namE); //16 (10+6)
-
+	     System.out.println(namE); // (10+6 = 16)
 	}
 
 }
